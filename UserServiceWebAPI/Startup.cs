c760a771811529206818b6a5d9f981d2f5fd25aa
Anton Log <Antuandelog@outlook.com>
2025-01-11 10:05:03 +0300

    refactor: changed ports, added stylecop, added startup to each service

---
+++ UserServiceWebAPI/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace UserServiceWebAPI
+{
+        public class Startup
+        {
+            public Startup(IConfiguration configuration)
+            {
+                Configuration = configuration;
+            }
+
+            public IConfiguration Configuration { get; }
+            public void ConfigureServices(WebApplicationBuilder builder)
+            {
+                builder.Services.AddControllers();
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+            }
+            public void Configure(WebApplication app, IWebHostEnvironment env, string[] args)
+            {
+                if(env.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+                app.UseAuthorization();
+            }
+        }
+}
+
